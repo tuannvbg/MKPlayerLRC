942859f8dfc12d3e87d39501a26214b2f5037bb4
Nguyễn Hữu Tuấn <nguyentuan2005@gmail.com>
2022-03-05 00:21:22 +0700

    Update musicList.js

--- js/musicList.js
+++ js/musicList.js
@@ -3,6 +3,9 @@
  * 播放列表配置模块
  * 编写：mengkun(http://mkblog.cn)
  * 时间：2017-9-15
+ * Edit: tuannvbg@gmail.com
+ * Date: 2022-03-04
+ * Target: use MKOnlineMusicPlayer with playlist use add .lrc dependent server
  *************************************************/
 // 建议修改前先备份一下
 // 获取 歌曲的网易云音乐ID 或 网易云歌单ID 的方法：
@@ -38,112 +41,121 @@ var musicList = [
     //*********************************************
     // 自定义列表开始，您可以自由添加您的自定义列表
     {
-        id: 3778678     // 云音乐热歌榜
+        name: "Nhạc Trẻ Tâm Trạng",   // 播放列表名字
+        cover: "https://photo-resize-zmp3.zadn.vn/w320_r1x1_webp/cover/4/d/f/4/4df47ce9d90a1c29acd2bbd647682b90.jpg",          // 播放列表封面
+        creatorName: "Tuấn",        // 列表创建者名字
+        creatorAvatar: "",      // 列表创建者头像
+        item: [
+			{
+                id: "001",  // 音乐ID
+                name: "Hoa Nở Không Màu",  // 音乐名字
+                artist: "Hoài Lâm", // 艺术家名字
+                album: "Nhạc Trẻ Tuyển Chọn",    // 专辑名字
+                source: "local",     // 音乐来源
+                url_id: "436514312",  // 链接ID
+                pic_id: "2946691234868155",  // 封面ID
+                lyric_id: "436514312",  // 歌词ID
+				lrc: "lyrics/Hoai.Lam-Hoa.No.Khong.Mau.lrc",
+                pic: "https://avatar-ex-swe.nixcdn.com/song/2020/05/15/c/f/3/0/1589532035884_500.jpg",    // 专辑图片
+                url: "https://dl.dropboxusercontent.com/s/si2mga7b5imfobl/Hoai.Lam-Hoa.No.Khong.Mau.mp3?dl=0"   // mp3链接（此项建议不填，除非你有该歌曲的比较稳定的外链）
+            },
+			{
+				id: "002",
+				name: "Buồn Làm Chi Em Ơi",
+				artist: "Hoài Lâm",
+				album: "Nhạc Trẻ Tuyển Chọn",
+				source: "local",
+				lrc: "lyrics/Hoai.Lam-Buon.Lam.Chi.Em.Oi.lrc",
+				pic: "https://avatar-ex-swe.nixcdn.com/song/2020/06/12/5/b/c/b/1591950830200_500.jpg",
+				url: "https://dl.dropboxusercontent.com/s/nc2x565rw5y5cgj/Hoai.Lam-Buon.Lam.Chi.Em.Oi.mp3?dl=0"
+			}
+		]
     },
     {
-        id: 3779629     // 云音乐新歌榜
-    },
-    {
-        id: 4395559     // 华语金曲榜
-    },
-    {
-        id: 64016     // 中国TOP排行榜（内地榜）
-    },
-    {
-        id: 112504     // 中国TOP排行榜（港台榜）
-    },
-    {
-        id: 19723756     // 云音乐飙升榜
-    },
-    {
-        id: 2884035     // "网易原创歌曲榜"
-    },
-    // 自定义列表教程开始！
-    // 方式一：手动创建列表并添加歌曲信息
-    // 温馨提示：各大音乐平台获取到的外链有效期均较短，因此 url 值应该设置为空，以让程序临时抓取
-    {
-        name: "自定义列表",   // 播放列表名字
-        cover: "https://p3.music.126.net/34YW1QtKxJ_3YnX9ZzKhzw==/2946691234868155.jpg", // 播放列表封面图像
+        name: "Nhạc Việt Nam 2022",   // 播放列表名字
+        cover: "https://photo-resize-zmp3.zadn.vn/w320_r1x1_webp/covers/f/3/f3ccdd27d2000e3f9255a7e3e2c48800_1519897848.jpg", // 播放列表封面图像
         creatorName: "",        // 列表创建者名字(暂时没用到，可空)
         creatorAvatar: "",      // 列表创建者头像(暂时没用到，可空)
         item: [                 // 这里面放歌曲
             {
-                id: "436514312",  // 音乐ID
-                name: "成都",  // 音乐名字
-                artist: "赵雷", // 艺术家名字
-                album: "成都",    // 专辑名字
-                source: "netease",     // 音乐来源
+                id: "001",  // 音乐ID
+                name: "Mỹ Nhân",  // 音乐名字
+                artist: "Đinh Đại Vũ", // 艺术家名字
+                album: "Nhạc Trẻ Tuyển Chọn",    // 专辑名字
+                source: "local",     // 音乐来源
                 url_id: "436514312",  // 链接ID
                 pic_id: "2946691234868155",  // 封面ID
                 lyric_id: "436514312",  // 歌词ID
-                pic: "https://p3.music.126.net/34YW1QtKxJ_3YnX9ZzKhzw==/2946691234868155.jpg",    // 专辑图片
-                url: ""   // mp3链接（此项建议不填，除非你有该歌曲的比较稳定的外链）
+				lrc: "lyrics/Dinh.Dai.Vu-My.Nhan.js",
+                pic: "https://avatar-ex-swe.nixcdn.com/song/2019/11/28/1/e/6/0/1574932935845.jpg",    // 专辑图片
+                url: "https://dl.dropboxusercontent.com/s/0d2ooyr1d3eg3s7/Dinh.Dai.Vu-My.Nhan.mp3"   // mp3链接（此项建议不填，除非你有该歌曲的比较稳定的外链）
+            }
+		]
+    },
+    
+    // 自定义列表教程开始！
+    // 方式一：手动创建列表并添加歌曲信息
+    // 温馨提示：各大音乐平台获取到的外链有效期均较短，因此 url 值应该设置为空，以让程序临时抓取
+    {
+        name: "Nhạc Việt Nam",   // 播放列表名字
+        cover: "https://photo-resize-zmp3.zadn.vn/w320_r1x1_webp/covers/f/3/f3ccdd27d2000e3f9255a7e3e2c48800_1519897848.jpg", // 播放列表封面图像
+        creatorName: "",        // 列表创建者名字(暂时没用到，可空)
+        creatorAvatar: "",      // 列表创建者头像(暂时没用到，可空)
+        item: [                 // 这里面放歌曲
+            {
+                id: "001",  // 音乐ID
+                name: "Mỹ Nhân",  // 音乐名字
+                artist: "Đinh Đại Vũ", // 艺术家名字
+                album: "Nhạc Trẻ Tuyển Chọn",    // 专辑名字
+                source: "local",     // 音乐来源
+                url_id: "436514312",  // 链接ID
+                pic_id: "2946691234868155",  // 封面ID
+                lyric_id: "436514312",  // 歌词ID
+				lrc: "lyrics/Dinh.Dai.Vu-My.Nhan.js",
+                pic: "https://avatar-ex-swe.nixcdn.com/song/2019/11/28/1/e/6/0/1574932935845.jpg",    // 专辑图片
+                url: "https://dl.dropboxusercontent.com/s/0d2ooyr1d3eg3s7/Dinh.Dai.Vu-My.Nhan.mp3"   // mp3链接（此项建议不填，除非你有该歌曲的比较稳定的外链）
             },
             // 下面演示插入各个平台的音乐。。。
             {
                 id: "65528",
-                name: "淘汰",
-                artist: "陈奕迅",
-                album: "认了吧",
-                source: "netease",      // 网易云
+                name: "Huynh Đệ À",
+                artist: "Đinh Đại Vũ",
+                album: "Nhạc Trẻ Tuyển Chọn",
+                source: "local",      // 网易云
                 url_id: "65528",
                 pic_id: "18782957139233959",
                 lyric_id: "65528",
-                pic: "https://p3.music.126.net/BFuOepLmD63tY75UJs1c0Q==/18872017579169120.jpg",
-                url: ""
+				lrc: "lyrics/Dinh.Dai.Vu-Huynh.De.A.json",
+				lrczmp3: "https://static-zmp3.zadn.vn/lyrics/1/1/7/c/117c480ea17fe29e30ce12a9a66da40e.lrc",
+                pic: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/9/8/d/7/98d7998a7c74ce85500fb19f8ede6150.jpg",
+                url: "https://dl.dropboxusercontent.com/s/whm7q7qd1l54wuc/Dinh.Dai.Vu-Huynh.De.A.mp3"
             },
             {
-                id: "001JD1SR29d1hS",
-                name: "特别的爱给特别的你",
-                artist: "伍思凯",
-                album: "特别的爱给特别的你",
-                source: "tencent",      // 腾讯
+                id: "003",
+                name: "Bước Qua Đời Nhau (Remix)",
+                artist: "Lê Bảo Bình",
+                album: "Việt Nam Remix",
+                source: "local",      // 腾讯
                 url_id: "001JD1SR29d1hS",
                 pic_id: "004DYsvN2QCYcj",
                 lyric_id: "001JD1SR29d1hS",
-                pic: "https://y.gtimg.cn/music/photo_new/T002R300x300M000004DYsvN2QCYcj.jpg?max_age=2592000",
-                url: ""     // 腾讯的外链有效期较短，插入时 url [必须]设置空值，播放时再临时抓取
+				lrc: "[00:00.12] Mà cớ sao ta giờ đây\n[00:02.00] Bước qua đời nhau\n[00:04.24] Rồi trái tim ta phải đau\n[00:06.12] Nhớ nhung vì nhau\n[00:08.37] Và có khi nào em thấy\n[00:10.24] Tiếc nuối về ngày xưa\n[00:12.86] Vội vàng quá để mình mất nhau\n[00:16.24] Ngày đấy giá anh và em\n[00:18.12] Chúng ta đừng cố chấp\n[00:20.37] Ngày đấy giá anh và em\n[00:22.25] Chúng ta bình tĩnh hơn\n[00:24.50] Ngày đấy giá anh kìm nén\n[00:26.03] Mỗi khi mình cãi vã\n[00:28.62] Để hai ta hai ta sẽ không\n[00:32.37] Bước qua đời nhau\n[01:04.75] Mình bước qua đời nhau\n[01:07.02] Để làm nhau đau\n[01:09.27] Để làm nhau khóc\n[01:10.38] Gặp nhau không muốn chào\n[01:12.64] Mình bước qua đời nhau\n[01:14.89] Để lại thương đau\n[01:17.14] Để lại vệt sâu\n[01:18.27] Có xóa mờ được đâu\n[01:20.13] Mình đã từng hạnh phúc\n[01:22.76] Phải không em ơi\n[01:24.64] Mình từng chung lối\n[01:26.14] Khổ đau lẫn tiếng cười\n[01:28.39] Mình đã luôn từng nói\n[01:30.63] À không thể chưa\n[01:32.88] Mình từng thế ước\n[01:34.39] Mãi không bao giờ xa\n[01:36.26] Mà cớ sao ta giờ đây\n[01:38.14] Bước qua đời nhau\n[01:40.38] Rồi trái tim ta phải đau\n[01:42.27] Nhớ nhung vì nhau\n[01:44.51] Và có khi nào em thấy\n[01:46.39] Tiếc nuối về ngày xưa\n[01:49.01] Vội vàng quá để mình mất nhau\n[01:52.02] Ngày đấy giá anh và em\n[01:54.27] Chúng ta đừng cố chấp\n[01:56.51] Ngày đấy giá anh và em\n[01:58.39] Chúng ta bình tĩnh hơn\n[02:00.26] Ngày đấy giá anh kìm nén\n[02:02.14] Mỗi khi mình cãi vã\n[02:04.77] Để hai ta hai ta sẽ không\n[02:08.51] Bước qua đời nhau\n[02:41.01] Mình bước qua đời nhau\n[02:42.89] Để làm nhau đau\n[02:45.14] Để làm nhau khóc\n[02:46.25] Gặp nhau không muốn chào\n[02:48.50] Mình bước qua đời nhau\n[02:50.76] Để lại thương đau\n[02:53.01] Để lại vệt sâu\n[02:54.15] Có xóa mờ được đâu\n[02:56.39] Mình đã từng hạnh phúc\n[02:59.00] Phải không em ơi\n[03:00.88] Mình từng chung lối\n[03:02.04] Khổ đau lẫn tiếng cười\n[03:04.26] Mình đã luôn từng nói\n[03:06.89] À không thể chưa\n[03:09.14] Mình từng thế ước\n[03:10.25] Mãi không bao giờ xa\n[03:12.13] Mà cớ sao ta giờ đây\n[03:14.03] Bước qua đời nhau\n[03:16.26] Rồi trái tim ta phải đau\n[03:18.14] Nhớ nhung vì nhau\n[03:20.39] Và có khi nào em thấy\n[03:22.25] Tiếc nuối về ngày xưa\n[03:24.88] Vội vàng quá để mình mất nhau\n[03:28.26] Ngày đấy giá anh và em\n[03:30.14] Chúng ta đừng cố chấp\n[03:32.38] Ngày đấy giá anh và em\n[03:34.25] Chúng ta bình tĩnh hơn\n[03:36.51] Ngày đấy giá anh kìm nén\n[03:38.37] Mỗi khi mình cãi vã\n[03:41.01] Để hai ta hai ta sẽ không\n[03:44.38] Bước qua đời nhau",
+                pic: "https://photo-resize-zmp3.zadn.vn/w240_r1x1_jpeg/cover/c/7/6/0/c76000d7918cc2494095343c9faea307.jpg",
+                url: "https://dl.dropboxusercontent.com/s/h81w2ojv42yv6tg/Phi.Nguyen-Buoc.Qua.Doi.Nhau.Remix.mp3"     // 腾讯的外链有效期较短，插入时 url [必须]设置空值，播放时再临时抓取
             },
             {
-                id: "81175",
-                name: "让我欢喜让我忧",
-                artist: "周华健",
-                album: "让我欢喜让我忧",
-                source: "xiami",    // 虾米
+                id: "004",
+                name: "Hoa Hải Đường",
+                artist: "Jack",
+                album: "The Best Of Jack",
+                source: "local",    // 虾米
                 url_id: "81175",
                 pic_id: "81175",
                 lyric_id: "81175",
-                pic: "https://pic.xiami.net/images/album/img58/1258/66271400572139.jpg@300h_300w_100q_1c.jpg",
-                url: ""     // 虾米的外链有效期较短，插入时 url [必须]设置空值，播放时再临时抓取
-            },
-            {
-                id: "2a24dea6c74884195fe5b9732fd95ca8",
-                name: "小幸运",
-                artist: "金玟岐",
-                album: "金玟岐翻唱作品集",
-                source: "kugou",        // 酷狗
-                url_id: "2a24dea6c74884195fe5b9732fd95ca8",
-                pic_id: "2a24dea6c74884195fe5b9732fd95ca8",
-                lyric_id: "2a24dea6c74884195fe5b9732fd95ca8",
-                pic: "http://singerimg.kugou.com/uploadpic/softhead/400/20161226/20161226105135733.jpg",
-                url: ""     // 酷狗的外链有效期较短，插入时 url [必须]设置空值，播放时再临时抓取
-            },
-            {
-                id: "121004737",
-                name: "难忘今宵",
-                artist: "李谷一",
-                album: "难忘今宵",
-                source: "baidu",        // 百度
-                url_id: "121004737",
-                pic_id: "121004737",
-                lyric_id: "121004737",
-                pic: "http://musicdata.baidu.com/data2/pic/2733cd9816b8618afd3038d5d9444940/266105319/266105319.jpg@s_0,w_150",
-                url: ""         // 百度的外链有效期较短，插入时 url [必须]设置空值，播放时再临时抓取
-            }  // 列表中最后一首歌大括号后面不要加逗号
+				lrc: "https://dl.dropboxusercontent.com/s/vfkvtpjysgre7v5/Jack-Hoa.Hai.Duong.lrc",
+                pic: "https://photo-resize-zmp3.zadn.vn/w480_r1x1_webp/cover/a/8/6/2/a8626a5671f5a01250a074c4c8140cae.jpg",
+                url: "https://dl.dropboxusercontent.com/s/dle05qjk3nidc83/Jack-Hoa.Hai.Duong.mp3?dl=0"     // 虾米的外链有效期较短，插入时 url [必须]设置空值，播放时再临时抓取
+            }
         ]
-    },
-    // 方式二：直接提供网易云歌单ID
-    {
-        id: 440103454   // 网易云歌单ID
-    }   // 播放列表的最后一项大括号后面不要加逗号
+    }  // 播放列表的最后一项大括号后面不要加逗号
 ];
